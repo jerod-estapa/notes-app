b19b6a181ef27f1c00107a90e2fef6ab122aa5eb
jerod <jerod@jerodestapa.com>
2019-01-04 00:45:30 -0600

    async/await refactor for lambda function in create.js

--- create.js
+++ create.js
@@ -1,9 +1,10 @@
 import uuid from "uuid"
-import AWS from "aws-sdk"
+import * as dynamoDbLib from "./libs/dynamodb-lib"
+import { success, failure } from "./libs/response-lib"
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient()
+// const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
-export function main(event, context, callback) {
+export async function main(event, context) {
     // Request body is passed in as a JSON encoded string in 'event.body'
     const data = JSON.parse(event.body)
 
@@ -26,30 +27,39 @@ export function main(event, context, callback) {
         }
     }
 
-    dynamoDb.put(params, (error, data) => {
-        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        }
+    // async/await refactor using libs
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);       
+    } catch (e) {
+        console.log(e)
+        return failure({ status: false })
+    }
 
-        // Return status code 500 on error
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify(({ status: false }))
-            }
-            callback(null, response)
-            return
-        }
+    // dynamoDb.put(params, (error, data) => {
+    //     // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    //     const headers = {
+    //         "Access-Control-Allow-Origin": "*",
+    //         "Access-Control-Allow-Credentials": true
+    //     }
 
-        // Return status code 200 and the newly created item
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        }
-        callback(null, response)
-    })
+    //     // Return status code 500 on error
+    //     if (error) {
+    //         const response = {
+    //             statusCode: 500,
+    //             headers: headers,
+    //             body: JSON.stringify(({ status: false }))
+    //         }
+    //         callback(null, response)
+    //         return
+    //     }
+
+    //     // Return status code 200 and the newly created item
+    //     const response = {
+    //         statusCode: 200,
+    //         headers: headers,
+    //         body: JSON.stringify(params.Item)
+    //     }
+    //     callback(null, response)
+    // })
 }
